685db463bab697df54a665e00315ecac4bf074d9
smiling girl.. sonrie   siempre  en la   vida <smiling girl.. sonrie   siempre  en la   vida>
2017-01-05 17:35:39 -0500

    practicando json

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var template="<div>{{lugar}}</div>"
+	$("#btn").on("click",function(){
+		$.getJSON("demo.json",function(value){
+			console.log(value);
+			var html=" ";
+			$.each(value.results,function(val,object){
+				console.log(val);
+				console.log(object);
+				var keys=object.classification;
+				html+=template.replace("{{nombre}}",keys);
+				console.log(template.replace("{{nombre}}",keys))
+
+				$(".mensaje").html(html);
+				console.log($(".contenedor").html(html));
+			});		
+		});
+	});
+});
